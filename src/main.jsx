3e72d3ad6c7ee045a362538cb512d39c9535be59
Muskan Singh <muskansingh@Muskans-MacBook-Air.local>
2025-01-19 18:03:19 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Footer from './components/footer/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/onlineCourse">
       <Navbar />  
       <App />     
       <Footer />
